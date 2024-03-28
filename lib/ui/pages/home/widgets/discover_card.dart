ed7fae85a8fe86b591669bd72ae586ed9d8ab66c
santtiagogp <santiago.gp004@gmail.com>
2024-03-28 00:12:16 -0500

    Animation added to Discover cards

--- lib/ui/pages/home/widgets/discover_card.dart
+++ lib/ui/pages/home/widgets/discover_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../foundation/app_theme.dart';
 
-class DiscoverCard extends StatelessWidget {
+class DiscoverCard extends StatefulWidget {
 
   const DiscoverCard({
     super.key,
@@ -18,8 +18,62 @@ class DiscoverCard extends StatelessWidget {
   final String price;
 
   @override
+  State<DiscoverCard> createState() => _DiscoverCardState();
+}
+
+class _DiscoverCardState extends State<DiscoverCard>
+  with SingleTickerProviderStateMixin {
+
+  late AnimationController controller;
+  late Animation<Offset> slide;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    slide = Tween<Offset>(
+      begin: const Offset(150.0, 0.0),
+      end: const Offset(0.0, 0.0)
+    ).animate(CurvedAnimation(
+      parent: controller,
+      curve: Curves.ease
+    ));
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
 
+    controller.forward();
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: slide.value,
+          child: DiscoverCardUI(
+            widget: widget
+          ),
+        );
+      }
+    );
+  }
+}
+
+class DiscoverCardUI extends StatelessWidget {
+  const DiscoverCardUI({
+    super.key,
+    required this.widget,
+  });
+
+  final DiscoverCard widget;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 10),
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
@@ -29,24 +83,23 @@ class DiscoverCard extends StatelessWidget {
       ),
       child: Row(
         children: [
-      
           Row(
             children: [
               CircleAvatar(
                 radius: 25,
-                backgroundImage: AssetImage(imagePath)
+                backgroundImage: AssetImage(widget.imagePath)
               ),
               const SizedBox(width: 10),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(title, style: const TextStyle(
+                  Text(widget.title, style: const TextStyle(
                     color: Colors.black,
                     fontSize: 16,
                     fontWeight: FontWeight.w600
                   )),
-                  Text(symbol, style: const TextStyle(color: Colors.black))
+                  Text(widget.symbol, style: const TextStyle(color: Colors.black))
                 ]
               )
             ]
@@ -56,7 +109,7 @@ class DiscoverCard extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Text(price, style: const TextStyle(
+                Text(widget.price, style: const TextStyle(
                   color: Colors.black,
                   fontSize: 18,
                   fontWeight: FontWeight.w600
@@ -73,7 +126,6 @@ class DiscoverCard extends StatelessWidget {
               ],
             ),
           )
-      
         ],
       ),
     );
